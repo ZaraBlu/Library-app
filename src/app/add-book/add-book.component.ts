c3bf983a16da217b97ced1b45a31ba5bc620011d
agri-bank\z-bikdeloo <z-bikdeloo@agri-bank.com>
2025-03-03 14:15:07 +0330

    add-book

--- src/app/add-book/add-book.component.ts
+++ src/app/add-book/add-book.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+import { BookService } from '../book.service';
+import { Book } from '../book.model';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-add-book',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './add-book.component.html',
   styleUrl: './add-book.component.css'
 })
 export class AddBookComponent {
+
+  book: Book = new Book();
+
+  constructor(private bookService: BookService) {
+
+  }
+
+  onSubmit() {
+    this.bookService.addBook(this.book).subscribe(() => {
+      console.log('Book Added Successfully!')
+    })
+  }
 
 }
--- src/app/book.service.ts
+++ src/app/book.service.ts
@@ -6,6 +6,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 
 
+
 @Injectable({
   providedIn: 'root'
 })
